5d60886643d9ecebce3f2ff6df21b4921d1570c5
dr0n <kornienko.andriy@gmail.com>
2021-05-19 17:25:57 +0300

    fix slider and filter

--- app/js/slider_filter.js
+++ app/js/slider_filter.js
@@ -8,72 +8,52 @@ $(document).ready(function(){
         $( '.swiper-filter a' ).removeClass( 'swiper-active' );
         $( this ).addClass( 'swiper-active' );
 
-        if (prayersSwiper) {
-            prayersSwiper.updateSize();
-            prayersSwiper.updateSlides();
-            prayersSwiper.updateProgress();
-            prayersSwiper.updateSlidesClasses();
-            prayersSwiper.slideTo(0);
-            prayersSwiper.scrollbar.updateSize();
+        if (mySwiper) {
+            mySwiper.updateSize();
+            mySwiper.updateSlides();
+            mySwiper.updateProgress();
+            mySwiper.updateSlidesClasses();
+            mySwiper.slideTo(0);
+            mySwiper.scrollbar.updateSize();
         }
 
         return false;
     });
 
-    // breakpoint where swiper will be destroyed
-    // and switches to a dual-column layout
-    const breakpoint = window.matchMedia( '(min-width:768px)' );
-
-    // keep track of swiper instances to destroy later
-    let prayersSwiper;
-
-    const breakpointChecker = function() {
-
-        // if larger viewport and multi-row layout needed
-        if ( breakpoint.matches === true ) {
-
-            // clean up old instances and inline styles when available
-            if ( prayersSwiper !== undefined ) prayersSwiper.destroy( true, true );
-
-            // or/and do nothing
-            return;
-
-            // else if a small viewport and single column layout needed
-        } else if ( breakpoint.matches === false ) {
-
-            // fire small viewport version of swiper
-            return enableSwiper();
-
+    var mySwiper = undefined;
+    function initSwiper() {
+        var screenWidth = $(window).width();
+        if(screenWidth < 992 && mySwiper == undefined) {
+            mySwiper = new Swiper('.swiper-prayers', {
+                observer: true,
+                        slidesPerView: 1,
+                        runCallbacksOnInit: true,
+                        spaceBetween: 30,
+                        navigation: {
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev',
+                        },
+                        pagination: {
+                            el: '.swiper-pagination',
+                            clickable : true,
+                        },
+                        updateOnImagesReady: true
+            });
+        } else if (screenWidth > 768 && mySwiper != undefined) {
+            mySwiper.destroy();
+            mySwiper = undefined;
+            jQuery('.swiper-wrapper').removeAttr('style');
+            jQuery('.swiper-slide').removeAttr('style');
         }
+    }
 
-    };
+//Swiper plugin initialization
+    initSwiper();
 
-    const enableSwiper = function() {
-
-        prayersSwiper = new Swiper ('.swiper-prayers', {
-            observer: true,
-            slidesPerView: 1,
-            runCallbacksOnInit: true,
-            spaceBetween: 30,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-                clickable : true,
-            },
-            updateOnImagesReady: true
-        })
-
-    };
-
-    // keep an eye on viewport size changes
-    breakpoint.addListener(breakpointChecker);
-
-    // kickstart
-    breakpointChecker();
+//Swiper plugin initialization on window resize
+    $(window).on('resize', function(){
+        initSwiper();
+    });
 
 
 });
